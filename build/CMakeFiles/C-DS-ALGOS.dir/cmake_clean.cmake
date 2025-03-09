6ff9951390face21ff15c25bf1761c7a9196a5a3
Katakam-Karthik-Reddy <k.karthikreddy2001@gmail.com>
2025-03-09 18:49:55 +0530

    completed queue and added cmake

---
+++ build/CMakeFiles/C-DS-ALGOS.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "C-DS-ALGOS"
+  "C-DS-ALGOS.pdb"
+  "CMakeFiles/C-DS-ALGOS.dir/src/Queue/Queue.c.o"
+  "CMakeFiles/C-DS-ALGOS.dir/src/Queue/Queue.c.o.d"
+  "CMakeFiles/C-DS-ALGOS.dir/src/Queue/main.c.o"
+  "CMakeFiles/C-DS-ALGOS.dir/src/Queue/main.c.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/C-DS-ALGOS.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
