f4bf986819f9554cfe506d380aaa9c7d766c41d3
karthik <58939636+Katakam-Karthik-Reddy@users.noreply.github.com>
2025-03-10 22:00:01 +0530

    Delete build directory

--- build/CMakeFiles/C-DS-ALGOS.dir/src/Searching/linearSearch.c.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/C-DS-ALGOS.dir/src/Searching/linearSearch.c.o: \
- /home/karthik/c/C-DS-Algos/src/Searching/linearSearch.c \
- /usr/include/stdc-predef.h \
- /home/karthik/c/C-DS-Algos/src/Searching/linearSearch.h
