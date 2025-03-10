3db74d437b1e93890673c86c78419e49a84475e4
Katakam-Karthik-Reddy <k.karthikreddy2001@gmail.com>
2025-03-10 21:58:30 +0530

    completed searching algorithms

---
+++ build/CMakeFiles/C-DS-ALGOS.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "C-DS-ALGOS"
+  "C-DS-ALGOS.pdb"
+  "CMakeFiles/C-DS-ALGOS.dir/src/Searching/TwoPointerSearch.c.o"
+  "CMakeFiles/C-DS-ALGOS.dir/src/Searching/TwoPointerSearch.c.o.d"
+  "CMakeFiles/C-DS-ALGOS.dir/src/Searching/binarySearch.c.o"
+  "CMakeFiles/C-DS-ALGOS.dir/src/Searching/binarySearch.c.o.d"
+  "CMakeFiles/C-DS-ALGOS.dir/src/Searching/linearSearch.c.o"
+  "CMakeFiles/C-DS-ALGOS.dir/src/Searching/linearSearch.c.o.d"
+  "CMakeFiles/C-DS-ALGOS.dir/src/Searching/main.c.o"
+  "CMakeFiles/C-DS-ALGOS.dir/src/Searching/main.c.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/C-DS-ALGOS.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/C-DS-ALGOS.dir/src/Searching/linearSearch.c.o.d
@@ -0,0 +1,4 @@
+CMakeFiles/C-DS-ALGOS.dir/src/Searching/linearSearch.c.o: \
+ /home/karthik/c/C-DS-Algos/src/Searching/linearSearch.c \
+ /usr/include/stdc-predef.h \
+ /home/karthik/c/C-DS-Algos/src/Searching/linearSearch.h
